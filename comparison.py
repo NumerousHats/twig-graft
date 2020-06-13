73d436e7179fedfe2c62a30c6791a8f1fd7b6f0c
Michael Andrec <mandrecphd@gmail.com>
2020-06-13 13:14:27 -0400

    Simplify name comparison logic

--- comparison.py
+++ comparison.py
@@ -48,6 +48,9 @@ def thing_match(thing1, thing2, total_count, total_comp, increment=1.0):
 
 
 def compare_name_part(name1: Name, name2: Name, part: str):
+    if not name1 or not name2:
+        return None
+
     if part == "given":
         standard1 = name1.standard_given
         standard2 = name2.standard_given
@@ -79,7 +82,7 @@ def compare_fullname(name1: Name, name2: Name, disqualify_surname_mismatch=False
         disqualify_surname_mismatch: if True, cause surname mismatch to return False
 
     Returns:
-        True if there is a match on both name parts, False if it is impossible for the two Names to be of the
+        True if there is a match on both name parts, False if it is impossible that the two Names belong to the
         same person-in-real-life, and None otherwise.
 
     """
@@ -106,85 +109,53 @@ def compare_fullname(name1: Name, name2: Name, disqualify_surname_mismatch=False
 
 
 def name_match(names1, names2):
-    logger = logging.getLogger(__name__)
-    # print(str(repr(names1)))
-    # print("\n")
-    # print(str(repr(names2)))
     matches = 0
     comparisons = 0
 
     if not names1 or not names2:
         return 0, 0
-    
-    if 'birth' in names1.keys():
-        name1 = names1["birth"][0]
-        if 'birth' in names2.keys():
-            name2 = names2["birth"][0]
-            comp = compare_fullname(name1, name2, disqualify_surname_mismatch=True)
+
+    birth1 = names1.pop("birth", [None])[0]
+    birth2 = names2.pop("birth", [None])[0]
+
+    if birth1 and birth2:
+        comp = compare_fullname(birth1, birth2, disqualify_surname_mismatch=True)
+        comparisons += 1
+        if comp:
+            matches += 1
+        elif comp is False:
+            return -1, 0
+
+    names1list = [item for sublist in [names1[k] for k in ("married", "unknown") if k in names1] for item in sublist]
+    names2list = [item for sublist in [names2[k] for k in ("married", "unknown") if k in names2] for item in sublist]
+
+    for name1 in names1list:
+        for name2 in names2list:
+            comp = compare_fullname(name1, name2)
+            comparisons += 1
             if comp:
                 matches += 1
             elif comp is False:
                 return -1, 0
-        if 'married' in names2.keys():
-            for name2 in names2["married"]:
-                comp = compare_fullname(name1, name2)
-                if comp:
-                    matches += 1
-                elif comp is False:
-                    return -1, 0
-        if 'unknown' in names2.keys():
-            for name2 in names2["unknown"]:
-                comp = compare_fullname(name1, name2)
-                if comp:
-                    matches += 1
-                elif comp is False:
-                    return -1, 0
-    elif 'married' in names1.keys():
-        for name1 in names1["married"]:
-            if 'birth' in names2.keys():
-                name2 = names2["birth"][0]
-                comp = compare_fullname(name1, name2, disqualify_surname_mismatch=True)
-                if comp:
-                    matches += 1
-                elif comp is False:
-                    return -1, 0
-            if 'married' in names2.keys():
-                for name2 in names2["married"]:
-                    comp = compare_fullname(name1, name2)
-                    if comp:
-                        matches += 1
-                    elif comp is False:
-                        return -1, 0
-            if 'unknown' in names2.keys():
-                for name2 in names2["unknown"]:
-                    comp = compare_fullname(name1, name2)
-                    if comp:
-                        matches += 1
-                    elif comp is False:
-                        return -1, 0
-    elif 'unknown' in names1.keys():
-        for name1 in names1["unknown"]:
-            if 'birth' in names2.keys():
-                name2 = names2["birth"][0]
-                comp = compare_fullname(name1, name2)
-                if comp:
-                    matches += 1
-                elif comp is False:
-                    return -1, 0
-            if 'married' in names2.keys():
-                for name2 in names2["married"]:
-                    comp = compare_fullname(name1, name2)
-                    if comp:
-                        matches += 1
-                    elif comp is False:
-                        return -1, 0
-            if 'unknown' in names2.keys():
-                for name2 in names2["unknown"]:
-                    comp = compare_fullname(name1, name2)
-                    if comp:
-                        matches += 1
-                    elif comp is False:
-                        return -1, 0
+
+    if birth1:
+        for name1 in names1list:
+            comp = compare_fullname(name1, birth2)
+            comparisons += 1
+            if comp:
+                matches += 1
+            elif comp is False:
+                return -1, 0
+
+    if birth2:
+        for name2 in names2list:
+            comp = compare_fullname(birth1, name2)
+            comparisons += 1
+            if comp:
+                matches += 1
+            elif comp is False:
+                return -1, 0
+
     return matches, comparisons
 
 
