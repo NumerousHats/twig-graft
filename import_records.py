f7a5a3f18408d198f35a93f9433264c6edb3e01b
Michael Andrec <mandrecphd@gmail.com>
2021-02-08 22:28:16 -0500

    Begin implementing birth record import - BROKEN

--- import_records.py
+++ import_records.py
@@ -278,6 +278,7 @@ class DeathRecord(Record):
                                     Name(name_type="birth", name_parts={"surname": self.recorded_name["surname"]},
                                          thesaurus=self.thesaurus),
                                     ["surname", "given_name"])
+            # TODO why would a stillbirth have a given name?
             return
 
         too_young_to_be_married = self.age.duration < datetime.timedelta(days=13) or \
@@ -647,6 +648,351 @@ def import_deaths(filename, graph, thesaurus):
                 # if row["second_marriage"]:
                 #     this_record.set_spouse(row["spouse_2"], row["spouse_2_surname"],
                 #                            row["widow(er)_2"], row["years_married_2"])
+            except (ParseError, InconsistentInputError):
+                logger.error("Row {} ({}) has inconsistent or malformed input".format(row_num, source))
+                continue
+
+            # print(this_record.decedent)
+            graph.append(this_record)
+
+
+class BirthRecord(Record):
+    def __init__(self, thesaurus, source, location, notes, confidence):
+        self.thesaurus = thesaurus
+        self.source = source
+        self.location = location
+        self.notes = notes
+        self.confidence = confidence
+
+        self.recorded_name = None
+        self.birth_date = None
+
+        self.newborn = Person(sources=self.source)
+
+        self.father = None
+        self.father_rel = None
+        self.mother = None
+        self.mother_rel = None
+        self.parent_rel = None
+
+        self.ff = None
+        self.f_ff_rel = None
+        self.fm = None
+        self.f_fm_rel = None
+        self.ff_fm_rel = None
+
+        self.fmf = None
+        self.fm_fmf_rel = None
+
+        self.mf = None
+        self.m_mf_rel = None
+        self.mm = None
+        self.m_mm_rel = None
+        self.mm_mf_rel = None
+
+        self.mmf = None
+        self.mm_mmf_rel = None
+
+        self.logger = logging.getLogger(__name__)
+        self.maximum_age = datetime.timedelta(days=365 * 110)
+
+    def __repr__(self):
+        return json.dumps(self.json(), indent=2)
+
+    def people(self):
+        all_people = [self.newborn, self.father, self.mother,
+                      self.ff, self.fm,
+                      self.mf, self.mm,
+                      self.fmf, self.mmf]
+        return [x for x in all_people if x]
+
+    def relations(self):
+        all_relations = [self.father_rel, self.mother_rel, self.parent_rel,
+                         self.f_ff_rel, self.f_fm_rel, self.ff_fm_rel, self.fm_fmf_rel,
+                         self.m_mf_rel, self.m_mm_rel, self.mm_mf_rel, self.mm_mmf_rel]
+        return [x for x in all_relations if x]
+
+    def json(self):
+        people_json = [x.json() for x in self.people()]
+        relations_json = [x.json() for x in self.relations()]
+        return {"people": people_json, "relations": relations_json}
+
+    def add_annotated_fact(self, person, fact, fields):
+        """Add a Fact, along with any associated notes and confidence values, to a Person or Relationship.
+
+        Args:
+            person (Person or Relationship): The object to which the Fact is to be added.
+            fact (Fact): The Fact to be added.
+            fields (list of str): The names of the record columns that are relevant to that fact.
+        """
+        try:
+            person.add_fact(
+                add_notes_confidence(fact, self.notes, self.confidence, fields))
+        except AttributeError:
+            raise ParseError
+
+    def add_annotated_name(self, person, name, fields):
+        """Add a Name, along with any associated notes and confidence values, to a Person.
+
+        Args:
+            person (Person): The Person object to which the Name is to be added.
+            name (Name): The Name to be added.
+            fields (list of str): The names of the record columns that are relevant to that fact.
+        """
+        person.add_name(
+            add_notes_confidence(name, self.notes, self.confidence, fields))
+
+    def set_newborn_names(self, surname_col, given_col, mf_surname_col, illegitimate):
+        """Set the names of the newborn. This is considerably simpler than the death record case, as the only
+            complicating factor is legitimization of illegitimate births.
+
+        Args:
+            surname_col (str or None): The contents of the "surname" column in the CSV file.
+            given_col (str or None): The contents of the "given_name" column in the CSV file.
+            mf_surname_col (str or None): The contents of the "m_father_surname" column of the CSV file.
+            illegitimate (bool): True if the birth was illegitimate.
+        """
+
+        self.recorded_name, aka_name, stillbirth = parse_name(surname_col, given_col)
+
+        if stillbirth:
+            self.newborn.add_fact(Fact(fact_type="Stillbirth", sources=self.source))
+            self.add_annotated_name(self.newborn,
+                                    Name(name_type="birth", name_parts={"surname": self.recorded_name["surname"]},
+                                         thesaurus=self.thesaurus),
+                                    ["surname", "given_name"])
+            # TODO why would a stillbirth have a given name?
+            return
+
+        if self.recorded_name:
+            self.add_annotated_name(self.newborn,
+                                    Name(name_type="birth", name_parts=self.recorded_name,
+                                         thesaurus=self.thesaurus),
+                                    ["surname", "given_name"])
+        if aka_name:
+            self.add_annotated_name(self.newborn,
+                                    Name(name_type="also_known_as", name_parts=aka_name, thesaurus=self.thesaurus),
+                                    ["surname", "given_name"])
+
+        # TODO deal with legitimized name
+
+    def set_birth_death(self, birth_date_col, baptism_date_col, year_col, death_date_col):
+        if birth_date_col:
+            self.birth_date = parse_date(birth_date_col, year_col)
+            self.add_annotated_fact(self.newborn,
+                                    Fact(fact_type="Birth", date=self.birth_date,
+                                         locations=self.location, sources=self.source),
+                                    ["birth_date", "year"])
+        else:
+            self.birth_date = None
+
+        if baptism_date_col:
+            baptism_date = parse_date(baptism_date_col, year_col)
+            self.add_annotated_fact(self.newborn,
+                                    Fact(fact_type="Baptism", date=baptism_date,
+                                         locations=self.location, sources=self.source),
+                                    ["baptism_date", "year"])
+        else:
+            baptism_date = None
+
+        if self.birth_date is None:
+            self.logger.error("Birth date missing. Nonstandard record?")
+            return
+
+        if death_date_col:
+            death = Fact(fact_type="Death", sources=self.source)
+            death_date = parse_date(death_date_col, year_col)
+            death.add_date(death_date)
+            self.add_annotated_fact(self.newborn, death, ["death_date"])
+
+    def set_parents(self, father_col, father_deceased, mother_col):
+        if father_col:
+            self.father = Person(gender="m",
+                                 names=Name(name_type="birth",
+                                            name_parts={"given": father_col,
+                                                        "surname": self.recorded_name["surname"]},
+                                            thesaurus=self.thesaurus),
+                                 sources=self.source)
+            self.father_rel = Relationship(self.father.identifier, self.newborn.identifier,
+                                           "parent-child", sources=self.source)
+
+            if father_deceased:
+                # TODO deal with deceased father
+                pass
+
+        if mother_col:
+            self.mother = Person(gender="f",
+                                 names=Name(name_type="married",
+                                            name_parts={"given": mother_col, "surname": self.recorded_name["surname"]},
+                                            thesaurus=self.thesaurus),
+                                 sources=self.source)
+            # TODO add maiden name if we have mf information
+
+            self.mother_rel = Relationship(self.mother.identifier, self.newborn.identifier,
+                                           "parent-child", sources=self.source)
+
+        if self.father and self.mother:
+            self.parent_rel = Relationship(self.father.identifier, self.mother.identifier, "spouse",
+                                           sources=self.source)
+
+    def set_father_ancestors(self, father, ff, fm, fmf_given, fmf_surname):
+        if mothers_father:
+            mf_name = parse_comma_name(mothers_father)
+        else:
+            mf_name = None
+
+        if mothers_mothers_father:
+            mmf_name = parse_comma_name(mothers_mothers_father)
+        else:
+            mmf_name = None
+
+        if mf_name:
+            self.mf = Person(gender="m", sources=self.source)
+            self.add_annotated_name(self.mf, Name(name_type="birth", name_parts=mf_name, thesaurus=self.thesaurus),
+                                    ["mothers_father"])
+            if self.mother:
+                self.m_mf_rel = Relationship(self.mf.identifier, self.mother.identifier, "parent-child",
+                                             sources=self.source)
+
+        if mothers_mother:
+            self.mm = Person(gender="f", sources=self.source)
+            self.add_annotated_name(self.mm,
+                                    Name(name_type="married",
+                                         name_parts={"given": mothers_mother,
+                                                     "surname": mf_name.get("surname", None)},
+                                         thesaurus=self.thesaurus),
+                                    ["mothers_mother", "mothers_father"])
+            if mmf_name and "surname" in mmf_name:
+                self.add_annotated_name(self.mm,
+                                        Name(name_type="birth",
+                                             name_parts={"given": mothers_mother,
+                                                         "surname": mmf_name.get("surname", None)},
+                                             thesaurus=self.thesaurus),
+                                        ["mothers_mother", "mothers_mothers_father"])
+            self.m_mm_rel = Relationship(self.mm.identifier, self.mother.identifier, "parent-child",
+                                         sources=self.source)
+
+        if self.mm and self.mf:
+            self.mm_mf_rel = Relationship(self.mm.identifier, self.mf.identifier, "spouse", sources=self.source)
+
+        if mmf_name and "given" in mmf_name:
+            # i.e. only create a maternal grandfather Person if we know his given name
+
+            self.mmf = Person(gender="m", sources=self.source)
+            self.add_annotated_name(self.mmf, Name(name_type="birth", name_parts=mmf_name, thesaurus=self.thesaurus),
+                                    ["mothers_mothers_father"])
+            self.mm_mmf_rel = Relationship(self.mmf.identifier, self.mm.identifier,
+                                           "parent-child", sources=self.source)
+
+        if mf_name and "surname" in mf_name and self.mother:
+            self.add_annotated_name(self.mother,
+                                    Name(name_type="birth",
+                                         name_parts={"given": mother, "surname": mf_name["surname"]},
+                                         thesaurus=self.thesaurus),
+                                    ["mother", "mothers_father"])
+
+    def set_mother_ancestors(self, mother, mothers_father, mothers_mother, mothers_mothers_father):
+        # TODO oh boy. lots of work needed here...
+
+        if mothers_father:
+            mf_name = parse_comma_name(mothers_father)
+        else:
+            mf_name = None
+
+        if mothers_mothers_father:
+            mmf_name = parse_comma_name(mothers_mothers_father)
+        else:
+            mmf_name = None
+
+        if mf_name:
+            self.mf = Person(gender="m", sources=self.source)
+            self.add_annotated_name(self.mf, Name(name_type="birth", name_parts=mf_name, thesaurus=self.thesaurus),
+                                    ["mothers_father"])
+            if self.mother:
+                self.m_mf_rel = Relationship(self.mf.identifier, self.mother.identifier, "parent-child",
+                                             sources=self.source)
+
+        if mothers_mother:
+            self.mm = Person(gender="f", sources=self.source)
+            self.add_annotated_name(self.mm,
+                                    Name(name_type="married",
+                                         name_parts={"given": mothers_mother,
+                                                     "surname": mf_name.get("surname", None)},
+                                         thesaurus=self.thesaurus),
+                                    ["mothers_mother", "mothers_father"])
+            if mmf_name and "surname" in mmf_name:
+                self.add_annotated_name(self.mm,
+                                        Name(name_type="birth",
+                                             name_parts={"given": mothers_mother,
+                                                         "surname": mmf_name.get("surname", None)},
+                                             thesaurus=self.thesaurus),
+                                        ["mothers_mother", "mothers_mothers_father"])
+            self.m_mm_rel = Relationship(self.mm.identifier, self.mother.identifier, "parent-child",
+                                         sources=self.source)
+
+        if self.mm and self.mf:
+            self.mm_mf_rel = Relationship(self.mm.identifier, self.mf.identifier, "spouse", sources=self.source)
+
+        if mmf_name and "given" in mmf_name:
+            # i.e. only create a maternal grandfather Person if we know his given name
+
+            self.mmf = Person(gender="m", sources=self.source)
+            self.add_annotated_name(self.mmf, Name(name_type="birth", name_parts=mmf_name, thesaurus=self.thesaurus),
+                                    ["mothers_mothers_father"])
+            self.mm_mmf_rel = Relationship(self.mmf.identifier, self.mm.identifier,
+                                           "parent-child", sources=self.source)
+
+        if mf_name and "surname" in mf_name and self.mother:
+            self.add_annotated_name(self.mother,
+                                    Name(name_type="birth",
+                                         name_parts={"given": mother, "surname": mf_name["surname"]},
+                                         thesaurus=self.thesaurus),
+                                    ["mother", "mothers_father"])
+
+
+def import_births(filename, graph, thesaurus):
+    logger = logging.getLogger(__name__)
+
+    with open(filename) as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=',', quotechar='"')
+        for row_num, row in enumerate(reader):
+            source = Source(repository=row["repository"], volume=row["book"], page_number=row["page"],
+                            entry_number=row["entry"], image_file=row["image"])
+            logger.debug("Doing {}".format(source))
+
+            if not row["surname"]:
+                logger.info(
+                    "Row {} ({}) has no surname and so is either empty or nonstandard. Skipping.".format(row_num,
+                                                                                                         source))
+                continue
+
+            notes, confidence = parse_notes(row)
+            # TODO there are major problems with doing it this way, namely that there is no way to attach a
+            #  confidence to a single component of a multiple-component entry (e.g. one that has both a given
+            #  and a surname)
+
+            location = Location(house_number=row["house_number"],
+                                alt_house_number=row["alt_house_number"],
+                                alt_village=row["house_location"])
+            add_notes_confidence(location, notes, confidence,
+                                 ["house_number", "alt_house_number", "house_location"])
+
+            this_record = BirthRecord(thesaurus, source, location, notes, confidence)
+            this_record.newborn.gender = row["gender"]  # TODO deal with uncertain gender
+
+            # TODO deal with primogenitus
+
+            try:
+                if row["illegitimate"] == "y":
+                    illegitimate = True
+                else:
+                    illegitimate = False
+                this_record.set_newborn_names(row["surname"], row["given_name"], row["m_father_surname"], illegitimate)
+                this_record.set_birth_death(row["birth_date"], row["baptism_date"], row["year"], row["death_date"])
+                this_record.set_parents(row["father"], row["father_deceased"], row["mother"])
+                this_record.set_father_ancestors(row["father"], row["f_father"], row["f_mother"],
+                                                 row["f_m_father_given"], row["f_m_father_surname"])
+
             except (ParseError, InconsistentInputError):
                 logger.error("Row {} ({}) has inconsistent or malformed input".format(row_num, source))
                 continue
